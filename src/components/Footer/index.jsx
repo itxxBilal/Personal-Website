1bb1cf15f44434954b3a74e8aa3f0fcb5c3e4e4d
Muhammad Bilal <119156465+itxxBilal@users.noreply.github.com>
2023-11-01 17:42:46 +0500

    Personal Website

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faLinkedin,
+  faGithub,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+
+const FooterContainer = styled.div`
+  width: 100%;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: center;
+  background: linear-gradient(
+    100.26deg,
+    rgba(0, 102, 255, 0.05) 42.33%,
+    rgba(150, 0, 225, 0.05) 127.07%
+  );
+`;
+
+const FooterWrapper = styled.footer`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+  align-items: center;
+  padding: 1rem;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 0.5rem;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+    text-align: center;
+    font-size: 12px;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  text-decoration: none;
+  font-size: 1.2rem;
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+const SocialMediaIcons = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`;
+
+const SocialMediaIcon = styled.a`
+  display: inline-block;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.text_primary};
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Copyright = styled.p`
+  margin-top: 1.5rem;
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.soft2};
+  text-align: center;
+`;
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <Nav>
+          <NavLink href="#about">Home</NavLink>
+          <NavLink href="https://www.youtube.com/@itx-bilal72" target="_blank">
+            YouTube
+          </NavLink>
+          <NavLink href="mailto:contact.bilalofficial@gmail.com">Contact Us</NavLink>
+        </Nav>
+        <SocialMediaIcons>
+          
+          <SocialMediaIcon href="https://www.facebook.com/m.bilal172">
+            <FontAwesomeIcon icon={faFacebook} />
+          </SocialMediaIcon>
+          <SocialMediaIcon href="https://twitter.com/itx_bilal172">
+            <FontAwesomeIcon icon={faXTwitter} />
+          </SocialMediaIcon>
+          <SocialMediaIcon href="https://github.com/itxxBilal">
+            <FontAwesomeIcon icon={faGithub} />
+          </SocialMediaIcon>
+          <SocialMediaIcon href="https://www.linkedin.com/in/bilal172/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </SocialMediaIcon>
+        </SocialMediaIcons>
+        <Copyright>&copy; {new Date().getFullYear()} Developed by Bilal</Copyright>
+      </FooterWrapper>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
---
+++ src/data/constants.jsx
@@ -0,0 +1,511 @@
+import FGC from "./FGC.jpg";
+import GOVT from "./GOVT.jpg";
+import PFTP from "./PFTP.jpg";
+import PIAIC from "./PIAIC.png";
+import ITXDEV from "./bg-logo-itx.png";
+import HOME from "./HO.avif";
+import  abeera from "./Projects/Abeera.png";
+import calligraphy from "./Projects/calligraphy.png";
+import Counter from "./Projects/Counter.png";
+import Vcard from "./Projects/v-Card Project.png";
+import Portfolio1 from "./Projects/Portfolio.png";
+import Movie from "./Projects/Movie.png";
+import PDF from "./Certi.png";
+
+
+export const Bio = {
+  name: "Muhammad Bilal",
+  roles: [
+    "Full Stack Developer",
+    "MERN-Stack Dev",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I'm Bilal, a passionate student learning web development. My goal is to become a full-stack developer, creating engaging and functional websites.",
+  github: "https://github.com/itxxbilal",
+  resume: "https://v-card-portfolio.vercel.app/",
+  linkedin: "https://www.linkedin.com/in/bilal172/",
+  twitter: "https://twitter.com/itx_bilal172",
+  insta: "https://www.instagram.com/itx_bilal_172/",
+  facebook: "https://www.facebook.com/m.bilal172",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Status_iucn_EX_icon.svg/480px-Status_iucn_EX_icon.svg.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ,
+    ],
+  },
+  // {
+  //   title: "Android",
+  //   skills: [
+  //     {
+  //       name: "Java",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //     },
+  //     {
+  //       name: "Kotlin",
+  //       image:
+  //         "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+  //     },
+  //     {
+  //       name: "XML",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+  //     },
+  //     {
+  //       name: "Android Studio",
+  //       image:
+  //         "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  //     },
+  //   ],
+  // },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/200px-GitHub_Invertocat_Logo.svg.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: ITXDEV,
+    role: "Web Developer",
+    company: "Itx-Developer",
+    date: "Dec 2022 - July 2023",
+    desc: "For the past 1.5 years, I've embarked on a self-guided journey into the world of coding from the comfort of my home. During this time, I've consistently honed my programming skills, exploring various languages and technologies. This self-discipline has allowed me to gain a deep understanding of software development and problem-solving. I'm determined to continue my learning and contribute to the ever-evolving tech landscape.",
+    skills: [ "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "NEXT.JS",
+    "ReactJS",
+    "ExpressJS",
+    "NodeJs",
+    "MongoDB",],
+    doc: calligraphy,
+  },
+  {
+    id: 0,
+    img: HOME,
+    role: "Devloper",
+    company: "at Home",
+    date: "Present",
+    desc: "Working and practicing at home on personal projects to improve skills.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "TypeScript",
+      "NEXT.JS",
+      "ReactJS",
+      "ExpressJS",
+      "NodeJs",
+      "MongoDB",
+    ],
+    doc: Movie,
+  },
+  {
+    id: 1,
+    img: PFTP,
+    role: "MERN-stack",
+    company: "PFTP Institute",
+    date: "11 Oct 2023",
+    desc: "I successfully completed my MERN Stack course, mastering HTML, CSS, JavaScript, React, Node, Express, and MongoDB. Ready for new challenges!",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ReactJS",
+      "jsx",
+      "NodeJs",
+      "MongoDB",
+      "ExpressJs",
+      "Postman",
+    ],
+    doc: PDF,
+  },
+  // {
+  //   id: 2,
+  //   img: "",
+  //   role: " ",
+  //   company: "",
+  //   date: "May 2023 - Present",
+  //   desc: "",
+  //   doc: "",
+  // },
+  // {
+  //   id: 3,
+  //   img: "",
+  //   role: "",
+  //   company: "",
+  //   date: "",
+  //   desc: "",
+  //   skills: [
+  //     "Leadership",
+  //     "",
+  //     "Kotlin",
+  //     "XML",
+  //     "Figma",
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   img: "",
+  //   role: "",
+  //   company: "",
+  //   date: "",
+  //   desc: "",
+  //   skills: [
+  //     "Android",
+  //     "Java",
+  //     "Kotlin",
+  //     "XML",
+  //     "Node Js",
+  //     "Cloud Firestore",
+  //     "Firebase",
+  //     "Figma",
+  //   ],
+  //   doc: "",
+  // },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: GOVT,
+    school: "Govt Hight School",
+    date: "Oct 2008 - Sep 2020",
+    grade: "814 Mark",
+    desc: "I've successfully completed my Matric with a focus on computer science, enhancing my fundamental knowledge in the field. Ready for further studies!",
+    degree: "(X) Matric Computer Science",
+  },
+  {
+    id: 1,
+    img: FGC,
+    school: "Frontier College",
+    date: "Nov 2021 - July 2023 ",
+    grade: "72.2%",
+    desc: "I've proudly completed my 12th Class at Frontier College in Chichawatni, where I specialized in computer science. This educational milestone has equipped me with a strong foundation for pursuing higher studies and a rewarding career in the ever-evolving world of technology.",
+    degree: "Inter(XII), With Computer Science",
+  },
+  {
+    id: 1,
+    img: PIAIC,
+    school: "PIAIC Institute",
+    date: "Nov 20 - Currently",
+    grade: "92.2%",
+    desc: "At PIAIC Institute, I'm immersed in a comprehensive coding curriculum, delving into Next.js, HTML, CSS, React, Redux, and an array of essential development tools. The program equips me with skills vital for modern web development, setting the stage for an exciting journey in the world of coding and technology.",
+    degree: "Web Development",
+  },
+
+  {
+    id: 3,
+    img: PFTP,
+    school: "PFTP - Professional Freelancing Training Program",
+    date: "OCT 11 2023",
+    grade: "80%",
+    desc: "I successfully completed my MERN Stack course, mastering HTML, CSS, JavaScript, React, Node, Express, and MongoDB. Ready for new challenges!",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Calligraphy-Website",
+    date: "Aug 2023",
+    description: "I created an elegant e-commerce site for calligraphy enthusiasts, featuring handcrafted products, smooth navigation, and secure online shopping, using HTML, CSS, and JavaScript.",
+    image: calligraphy,
+    tags: ["HTML", "CSS","JAVASCRIPT"],
+    category: "web app",
+    github: "https://github.com/itxxBilal/Calligraphy-site",
+    webapp: "https://calligraphy-site.vercel.app/",
+    member: [
+      {
+        name: "Muhammad Bilal",
+        img: "https://media.licdn.com/dms/image/D4D03AQGq-knicwkXbg/profile-displayphoto-shrink_200_200/0/1677837315133?e=1701302400&v=beta&t=HoCfYjHNcx_3WUPd8z7fyLESe0FWJU8F31_mNdM9Cok",
+        linkedin: "https://www.linkedin.com/in/bilal172//",
+        github: "https://github.com/itxxBilal/",
+      },
+      // {
+      //   name: "",
+      //   img: "",
+      //   linkedin: "https://www.linkedin.com/in/bilal172/",
+      //   github: "https://github.com/itxxBilal/",
+      // },
+    ],
+  },
+  {
+    id: 9,
+    title: "Abeera's Portfolio",
+    date: "Sep 2023",
+    description:
+    "I built a React portfolio site for my friend, skillfully presenting their talents, while also showcasing my own development skills.",
+    image: abeera,
+    tags: ["HTML5", "CSS", "JS","ReactJs","JSX"],
+    category: "web app",
+    github: "https://github.com/itxxBilal/Abeers-React-Portfolio",
+    webapp: "https://abeera.vercel.app/",
+  },
+  
+  {
+    id: 1,
+    title: "Counter App",
+    date: "Aug 2023",
+    description:
+    "I'm Bilal, a web developer. I've built a React counter app that delivers accurate counting and smooth performance, showcasing my expertise in coding and user-friendly design.",
+    image: Counter,
+    tags: ["HTML", "CSS","JavaScrpit","React Js" ],
+    category: "web app",
+    github: "https://github.com/itxxBilal/Counter-App",
+    webapp: "https://counter-app-eight-dusky.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Portfolio Site",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "I designed a minimalist portfolio website using HTML, CSS, and JavaScript to display my work, skills, and contact information stylishly, showcasing my frontend prowess.",
+    image: Portfolio1,
+    tags: ["HTML", "CSS","JAVASCRIPT"],
+    category: "machine learning",
+    github: "https://github.com/itxxBilal/portfolio-website",
+    webapp: "https://itxxbilal.github.io/portfolio-website/",
+    member: [
+      {
+        name: "Muhammad Bilal",
+        img: "https://media.licdn.com/dms/image/D4D03AQGq-knicwkXbg/profile-displayphoto-shrink_200_200/0/1677837315133?e=1701302400&v=beta&t=HoCfYjHNcx_3WUPd8z7fyLESe0FWJU8F31_mNdM9Cok",
+        linkedin: "https://www.linkedin.com/in/bilal172//",
+        github: "https://github.com/itxxBilal/",
+      },
+      // {
+      //   name: "Muhammad Bilal",
+      //   img: "",
+      //   linkedin: "https://www.linkedin.com/in/bilal172//",
+      //   github: "https://github.com/itxxBilal/",
+      // },
+    ],
+  },
+  {
+    id: 3,
+    title: "V-Card",
+    date: "Sep 2023",
+    description: "I created a V-card resume website using HTML, CSS, and JavaScript to showcase my skills, experience, and achievements professionally and attractively.",
+    image: Vcard,
+    tags: ["HTML", "CSS","JAVASCRIPT"],
+    category: "android app",
+    github: "https://github.com/itxxBilal/v-Card-Portfolio",
+    webapp: "https://v-card-portfolio.vercel.app/",
+    member: [
+      {
+        name: "Muhammad Bilal",
+        img: "https://media.licdn.com/dms/image/D4D03AQGq-knicwkXbg/profile-displayphoto-shrink_200_200/0/1677837315133?e=1701302400&v=beta&t=HoCfYjHNcx_3WUPd8z7fyLESe0FWJU8F31_mNdM9Cok",
+        linkedin: "https://www.linkedin.com/in/bilal172//",
+        github: "https://github.com/itxxBilal/",
+      },
+    ],
+  },
+  {
+    id: 10,
+    title: "Movie Website Clone",
+    date: "Oct 2023",
+    description: "I developed a responsive movie clone website in React, leveraging APIs to display film data, offering an interactive cinematic experience.",
+    image: Movie,
+    tags: ["HTML", "CSS","JavaScript","Reactjs"],
+    category: "android app",
+    github: "https://github.com/itxxBilal/React-movieclone-Project",
+    webapp: "https://react-movieclone-project.vercel.app/",
+    member: [
+      {
+        name: "Muhammad Bilal",
+        img: "https://media.licdn.com/dms/image/D4D03AQGq-knicwkXbg/profile-displayphoto-shrink_200_200/0/1677837315133?e=1701302400&v=beta&t=HoCfYjHNcx_3WUPd8z7fyLESe0FWJU8F31_mNdM9Cok",
+        linkedin: "https://www.linkedin.com/in/bilal172//",
+        github: "https://github.com/itxxBilal/",
+      },
+    ],
+  },
+  // {
+  //   id: 4,
+  //   title: "",
+  //   date: "",
+  //   description:
+  //     "",
+  //   image:
+  //     "",
+  //   tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+  //   category: "web app",
+  //   github: "",
+  //   webapp: "",
+  // },
+  // {
+  //   id: 5,
+  //   title: "",
+  //   date: "Jun 2021",
+  //   description:
+  //     " ",
+  //   image:
+  //     "",
+  //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+  //   category: "",
+  //   github: "",
+  //   webapp: "",
+  // },
+  // {
+  //   id: 6,
+  //   title: "",
+  //   date: "Jun 2021",
+  //   description:
+  //     "",
+  //   image:
+  //     "",
+  //   tags: ["React Js", "API", "Axios", "Node JS"],
+  //   category: "web app",
+  //   github: "",
+  //   webapp: "",
+  // },
+  // {
+  //   id: 7,
+  //   title: "",
+  //   date: "",
+  //   description:
+  //     "",
+  //   image:
+  //     "",
+  //   tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
+  //   category: "",
+  //   github: "",
+  //   webapp: "",
+  // },
+  // {
+  //   id: 8,
+  //   title: "",
+  //   date: "Jan 2021",
+  //   description:
+  //     "",
+  //   image:
+  //     "",
+  //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+  //   category: "machine learning",
+  //   github: "",
+  //   webapp: "",
+  // },
+];
+
